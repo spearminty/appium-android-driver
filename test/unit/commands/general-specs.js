3d3959a361517fd8b93ac042dae5c728d445972e
spearminty <chris.normand7@gmail.com>
2016-07-01 13:15:04 -0500

    merge updates from upstream to master (#1)
    * Add eslint and fix problems
    
    * Add androidInstallTimeout capability.
    
    Replace REMOTE_INSTALL_TIMEOUT with androidInstallTimeout capability.
    
    * 1.10.11
    
    * Use correct capability arguments
    
    installApkRemotely to parse correct capabilities from opts
    
    * Honor reset without the app capability
    
    Fixes #157
    
    * 1.10.12

--- test/unit/commands/general-specs.js
+++ test/unit/commands/general-specs.js
@@ -13,7 +13,7 @@ describe('General', () => {
     it('should throw an error if no keyboard is present', async () => {
       driver = new AndroidDriver();
       driver.adb = {};
-      driver.adb.isSoftKeyboardPresent = function(){ return false; };
+      driver.adb.isSoftKeyboardPresent = () => { return false; };
       await driver.hideKeyboard().should.be.rejectedWith(/not present/);
     });
     it('should throw an error if there is no selector', () => {
@@ -26,11 +26,36 @@ describe('General', () => {
       await driver.installApp('non/existent/app.apk').should.be.rejectedWith(/Could not find/);
     });
   });
+  describe('Run installed App', withMocks({helpers}, (mocks) => {
+    it('should throw error if run with full reset', async () => {
+      driver = new AndroidDriver();
+      driver.opts = {appPackage: "app.package", appActivity: "act", fullReset: true};
+      driver.caps = {};
+      await driver.initAUT().should.be.rejectedWith(/Full reset requires an app capability/);
+    });
+    it('should reset if run with fast reset', async () => {
+      driver = new AndroidDriver();
+      driver.opts = {appPackage: "app.package", appActivity: "act", fullReset: false, fastReset: true};
+      driver.caps = {};
+      driver.adb = "mock_adb";
+      mocks.helpers.expects("resetApp").withExactArgs("mock_adb", undefined, "app.package", true);
+      await driver.initAUT();
+      mocks.helpers.verify();
+    });
+    it('should keep data if run without reset', async () => {
+      driver = new AndroidDriver();
+      driver.opts = {appPackage: "app.package", appActivity: "act", fullReset: false, fastReset: false};
+      driver.caps = {};
+      mocks.helpers.expects("resetApp").never();
+      await driver.initAUT();
+      mocks.helpers.verify();
+    });
+  }));
   describe('getStrings', withMocks({helpers}, (mocks) => {
     it('should return app strings', async () => {
       driver = new AndroidDriver();
       driver.bootstrap = {};
-      driver.bootstrap.sendAction = function(){ return ''; };
+      driver.bootstrap.sendAction = () => { return ''; };
       mocks.helpers.expects("pushStrings")
           .returns({'test': 'en_value'});
       let strings = await driver.getStrings('en');
@@ -40,7 +65,7 @@ describe('General', () => {
     it('should return cached app strings for the specified language', async () => {
       driver = new AndroidDriver();
       driver.adb = {};
-      driver.adb.getDeviceLanguage = function(){ return 'en'; };
+      driver.adb.getDeviceLanguage = () => { return 'en'; };
       driver.apkStrings.en = {'test': 'en_value'};
       driver.apkStrings.fr = {'test': 'fr_value'};
       let strings = await driver.getStrings('fr');
@@ -49,7 +74,7 @@ describe('General', () => {
     it('should return cached app strings for the device language', async () => {
       driver = new AndroidDriver();
       driver.adb = {};
-      driver.adb.getDeviceLanguage = function(){ return 'en'; };
+      driver.adb.getDeviceLanguage = () => { return 'en'; };
       driver.apkStrings.en = {'test': 'en_value'};
       driver.apkStrings.fr = {'test': 'fr_value'};
       let strings = await driver.getStrings();
